53d38d884f8d630ec177327c5b1b7e06f5212567
Ruben Schuller <ruben@orgizm.net>
2015-09-12 16:24:38 +0200

    added own type for modification codes and stringer for it

--- modify.go
+++ modify.go
@@ -5,67 +5,23 @@ import (
 	"github.com/rbns/asn1-ber"
 )
 
-const (
-	ModAdd       = 0
-	ModDelete    = 1
-	ModReplace   = 2
-	ModIncrement = 3
-)
-
-var ModMap map[uint8]string = map[uint8]string{
-	ModAdd:       "add",
-	ModDelete:    "delete",
-	ModReplace:   "replace",
-	ModIncrement: "increment",
-}
-
-/* Reuse search struct, should Values be a [][]byte
-type EntryAttribute struct {
-	Name   string
-	Values []string
-}
-*/
 type Mod struct {
-	ModOperation uint8
+	ModOperation ModificationCode
 	Modification EntryAttribute
 }
 
+// LDAP modify request [https://tools.ietf.org/html/rfc4511#section-4.6]
 type ModifyRequest struct {
+	// DN of entry that is modified
 	DN       string
+	
+	// Changes
 	Mods     []Mod
+
+	// Server controls
 	Controls []Control
 }
 
-func (req *ModifyRequest) RecordType() uint8 {
-	return ModifyRecord
-}
-
-/* Example...
-func modifyTest(l *ldap.Conn){
-    var modDNs []string = []string{"cn=test,ou=People,dc=example,dc=com"}
-    var modAttrs []string = []string{"cn"}
-    var modValues []string = []string{"aaa", "bbb", "ccc"}
-	modreq := ldap.NewModifyRequest(modDNs[0])
-	mod := ldap.NewMod(ldap.ModAdd, modAttrs[0], modValues)
-	modreq.AddMod(mod)
-    err := l.Modify(modreq)
-	if err != nil {
-        fmt.Printf("Modify : %s : result = %d\n",modDNs[0],err.ResultCode)
-        return
-    }
-    fmt.Printf("Modify Success")
-}
-
-   ModifyRequest ::= [APPLICATION 6] SEQUENCE {
-         object          LDAPDN,
-         changes         SEQUENCE OF change SEQUENCE {
-              operation       ENUMERATED {
-                   add     (0),
-                   delete  (1),
-                   replace (2),
-                   ...  },
-              modification    PartialAttribute } }
-*/
 func (l *Connection) Modify(modReq *ModifyRequest) error {
 	messageID, ok := l.nextMessageID()
 	if !ok {
@@ -91,7 +47,7 @@ func encodeModifyRequest(req *ModifyRequest) (p *ber.Packet) {
 	seqOfChanges := ber.Encode(ber.ClassUniversal, ber.TypeConstructed, ber.TagSequence, nil, "Changes")
 	for _, mod := range req.Mods {
 		modification := ber.Encode(ber.ClassUniversal, ber.TypeConstructed, ber.TagSequence, nil, "Modification")
-		op := ber.NewInteger(ber.ClassUniversal, ber.TypePrimative, ber.TagEnumerated, uint64(mod.ModOperation), "Modify Op ("+ModMap[mod.ModOperation]+")")
+		op := ber.NewInteger(ber.ClassUniversal, ber.TypePrimative, ber.TagEnumerated, uint64(mod.ModOperation), "Modify Op")
 		modification.AppendChild(op)
 		partAttr := ber.Encode(ber.ClassUniversal, ber.TypeConstructed, ber.TagSequence, nil, "PartialAttribute")
 
@@ -131,7 +87,7 @@ func (req *ModifyRequest) String() (dump string) {
 
 // Basic LDIF dump, no formating, etc
 func (mod *Mod) DumpMod() (dump string) {
-	dump += fmt.Sprintf("%s: %s\n", ModMap[mod.ModOperation], mod.Modification.Name)
+	dump += fmt.Sprintf("%s: %s\n", mod.ModOperation.String(), mod.Modification.Name)
 	for _, val := range mod.Modification.Values {
 		dump += fmt.Sprintf("%s: %s\n", mod.Modification.Name, val)
 	}
@@ -139,7 +95,7 @@ func (mod *Mod) DumpMod() (dump string) {
 	return dump
 }
 
-func NewMod(modType uint8, attr string, values []string) (mod *Mod) {
+func NewMod(modType ModificationCode, attr string, values []string) (mod *Mod) {
 	if values == nil {
 		values = []string{}
 	}
